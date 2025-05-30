1dcc682111a34e2dfbbd7df946c8bbdb1d036ca8
Oleksandra Ovcharova <oleksaovcharova@gmail.com>
2025-05-30 22:04:05 +0200

    delete persist storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
-import { store, persistor } from "./redux/store.js";
-import { PersistGate } from "redux-persist/integration/react";
+import { store } from "./redux/store.js";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+      <App />
     </Provider>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-const persistConfig = {
-  key: "root",
-  storage,
-};
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(persistConfig, contactsReducer),
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
 });
-
-export const persistor = persistStore(store);
